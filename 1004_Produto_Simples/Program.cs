24c89a76185341859234ed1753f69716939f6692
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-04 18:07:55 -0300

    1004

---
+++ 1004_Produto_Simples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1004_Produto_Simples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = int.Parse(Console.ReadLine());
+            var B = int.Parse(Console.ReadLine());
+
+            // 27 = 3 * 9
+            var PROD = A * B;
+
+            Console.WriteLine("PROD = " + PROD);
+        }
+    }
+}
