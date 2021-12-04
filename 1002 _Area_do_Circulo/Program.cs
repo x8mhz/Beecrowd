30f619a0d7a1896ecf6f4237e028d144a44452d8
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-04 17:57:41 -0300

    1002

---
+++ 1002 _Area_do_Circulo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1002__Area_do_Circulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double n = 3.14159;
+            var raio = double.Parse(Console.ReadLine());
+
+            // 12.5664 = 3.14159 * (2.00 * 2.00)
+            var area = n * Math.Pow(raio, 2);
+
+            Console.WriteLine("A=" + area.ToString("N4").Replace(",", ""));
+        }
+    }
+}
