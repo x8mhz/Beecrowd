97084e571e3158891596ee3826491812e54eb0d8
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-04 20:34:41 -0300

    1007

---
+++ 1007_Diferenca/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1007_Diferenca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = int.Parse(Console.ReadLine());  
+            var B = int.Parse(Console.ReadLine());
+            var C = int.Parse(Console.ReadLine());
+            var D = int.Parse(Console.ReadLine());
+
+            // -26 = (5 * 6) - (7 * 8)
+            var DIFERENCA = (A * B) - (C * D);
+
+            Console.WriteLine("DIFERENCA = " + DIFERENCA);
+        }
+    }
+}
