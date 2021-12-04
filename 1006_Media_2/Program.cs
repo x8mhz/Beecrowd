c290a1233aa3a8e8eb5a6a3a8a2f1ac68d9c6d96
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-04 18:36:16 -0300

    1006

---
+++ 1006_Media_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1006_Media_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = double.Parse(Console.ReadLine());
+            var B = double.Parse(Console.ReadLine());
+            var C = double.Parse(Console.ReadLine());
+
+            // 6.3 = ((5.0 * 2) + (6.0 * 3) + (7.0 * 5)) / 10
+            var MEDIA = ((A * 2) + (B * 3) + (C * 5)) / 10;
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("N1"));
+        }
+    }
+}
