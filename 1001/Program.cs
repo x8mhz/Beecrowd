7abf11999e79c3b61da826c2e562f62e80d3b58e
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-04 17:50:07 -0300

    Adicionar arquivos de projeto.

---
+++ 1001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = int.Parse(Console.ReadLine());
+            var B = int.Parse(Console.ReadLine());
+
+            // 19 = 10 + 9
+            var X = A + B;
+
+            Console.WriteLine("X = " + X);
+        }
+    }
+}
