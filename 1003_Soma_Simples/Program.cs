e1ad73f7ed329c89f8c8abe0d67267a0a7ed5546
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-04 18:03:25 -0300

    1003

---
+++ 1003_Soma_Simples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1003_Soma_Simples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = int.Parse(Console.ReadLine());
+            var B = int.Parse(Console.ReadLine());
+
+            // 40 = 30 + 10
+            var SOMA = A + B;
+
+            Console.WriteLine("SOMA = " + SOMA);
+        }
+    }
+}
