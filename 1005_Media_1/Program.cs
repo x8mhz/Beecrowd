8587f3e0e20b8b84e4ca5f3d5f451094f61a40a8
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-04 18:22:48 -0300

    1005

---
+++ 1005_Media_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1005_Media_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = double.Parse(Console.ReadLine());
+            var B = double.Parse(Console.ReadLine());
+
+            // 6.43182 = ((A * 3.5) + (B * 7.5)) / 11
+            var MEDIA = ((A * 3.5) + (B * 7.5)) / 11;
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("N5"));
+        }
+    }
+}
