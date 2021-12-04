1268d70bb8cff909d722817ee9a1ddb2de3ac500
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-04 18:26:48 -0300

    1005 -  redacted

--- 1005_Media_1/Program.cs
+++ 1005_Media_1/Program.cs
@@ -9,7 +9,7 @@ namespace _1005_Media_1
             var A = double.Parse(Console.ReadLine());
             var B = double.Parse(Console.ReadLine());
 
-            // 6.43182 = ((A * 3.5) + (B * 7.5)) / 11
+            // 6.43182 = ((5.0 * 3.5) + (7.1 * 7.5)) / 11
             var MEDIA = ((A * 3.5) + (B * 7.5)) / 11;
 
             Console.WriteLine("MEDIA = " + MEDIA.ToString("N5"));
