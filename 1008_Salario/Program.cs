e3b2e5e60ae914e867eba90a6963a1055b1238a7
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-04 21:03:40 -0300

    1008

---
+++ 1008_Salario/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1008_Salario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerNumber = int.Parse(Console.ReadLine());
+            var TotalHoursWorked = int.Parse(Console.ReadLine());
+            var hourlySalary = double.Parse(Console.ReadLine());
+
+            // 550.00 = 100 * 5.50
+            var salary = TotalHoursWorked * hourlySalary;
+
+            Console.WriteLine("NUMBER = " + customerNumber);
+            Console.WriteLine("SALARY = U$ " + salary.ToString("N2").Replace(",", ""));
+        }
+    }
+}
