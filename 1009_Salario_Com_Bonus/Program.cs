ce193517da36d8058f70d70406616a3583b491e9
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-04 22:22:01 -0300

    1009

---
+++ 1009_Salario_Com_Bonus/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1009_Salario_Com_Bonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var seller = Console.ReadLine();
+            var salary = double.Parse(Console.ReadLine());
+            var totalSellMonthly = double.Parse(Console.ReadLine());
+
+            // 684.54 = (1230.30 * 15) / 100 + 500.00
+            salary = (totalSellMonthly * 15) / 100 + salary;
+
+            Console.WriteLine("TOTAL = R$ " + salary.ToString("N2").Replace(",", ""));
+        }
+    }
+}
