a71f87af9e7e913f83b566df01517ca569055b99
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-07 18:47:55 -0300

    1010

---
+++ 1010_Calculo_Simples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1010_Calculo_Simples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var item1 = Console.ReadLine().Split(' ');
+            var item2 = Console.ReadLine().Split(' ');
+
+            // 51.40 = (2 * 15.30) + (4 * 5.20) 
+            var itemPrice = (int.Parse(item1[1]) * double.Parse(item1[2])) + (int.Parse(item2[1]) * double.Parse(item2[2]));
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + itemPrice.ToString("F2"));
+        }
+    }
+}
