19c060852a08906b591de799eabc5b079da619dd
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-07 22:20:40 -0300

    1011

---
+++ 1011_Esfera/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1011_Esfera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+            var r = double.Parse(Console.ReadLine());
+
+            var v = (4 / 3.0) * pi * Math.Pow(r, 3);
+
+            Console.WriteLine("VOLUME = " + v.ToString("F3"));
+        }
+    }
+}
