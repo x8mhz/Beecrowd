a9b079473540cd07aebfa21c6518c3c71d6244c7
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-09 19:00:05 -0300

    1016

---
+++ 1016_Distancia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1016_Distancia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var X_kmh = 60 ;
+            var y_kmh = 90;
+
+            // 30km/h = 90km/h - 60km/h
+            y_kmh -= X_kmh;
+
+            // 220km/h = (60min * 110km) / 30km/h
+            y_kmh = (60 * int.Parse(Console.ReadLine())) / y_kmh;
+
+            Console.WriteLine(y_kmh + " minutos");
+        }
+    }
+}
