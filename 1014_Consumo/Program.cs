faa1b2686368abc9af0cbe0878f741ef432f1cbf
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-08 23:38:21 -0300

    1014

---
+++ 1014_Consumo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1014_Consumo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var X = int.Parse(Console.ReadLine());
+            var Y = double.Parse(Console.ReadLine());
+
+            //14.286 = 500 / 35.0
+            var kml = X / Y;
+
+            Console.WriteLine(kml.ToString("F3") + " km/l");
+        }
+    }
+}
---
+++ 1015_Distancia_Entre_Dois_Pontos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1015_Distancia_Entre_Dois_Pontos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = Console.ReadLine().Split();
+            var p2 = Console.ReadLine().Split();
+
+            var x1 = double.Parse(p1[0]);
+            var y1 = double.Parse(p1[1]);
+            var x2 = double.Parse(p2[0]);
+            var y2 = double.Parse(p2[1]);
+
+            var distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+            Console.WriteLine(distance.ToString("F4"));
+        }
+    }
+}
