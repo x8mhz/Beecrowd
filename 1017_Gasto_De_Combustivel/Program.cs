684f8ef6b7b9b65bbb68047a72bef408a27799c0
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-09 21:04:48 -0300

    1017

---
+++ 1017_Gasto_De_Combustivel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1017_Gasto_De_Combustivel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double kml = 12;
+
+            var t = int.Parse(Console.ReadLine());
+            var v = int.Parse(Console.ReadLine());
+
+            // 70.833... = (10 * 85) / 12
+            var l = Convert.ToDouble((t * v) / kml);
+
+            Console.WriteLine(l.ToString("F3"));
+        }
+    }
+}
