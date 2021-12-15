75844e154757b569570b19bd27d6fbc67dcaec35
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-14 21:40:17 -0300

    1019

---
+++ 1019_Conversao_De_Tempo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1019_Conversao_De_Tempo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+
+            // 53 = (140153 % 3600) % 60
+            var S = (N % 3600) % 60;
+            // 55 = (140153 %) / 06
+            var M = (N % 3600) / 60;
+            // 38 = 140153 / 3600
+            var H = N / 3600;
+
+            Console.WriteLine(H + ":" + M + ":" + S);
+        }
+    }
+}
---
+++ SAPContest_1_CruzamentoBloqueado/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace SAPContest_1_CruzamentoBloqueado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dim1 = Console.ReadLine().Split();
+            var dim2 = Console.ReadLine().Split();
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var item1 in dim1[i])
+                {
+                    if (Convert.ToInt32(item1) >= 0)
+                    {
+                        foreach (var item2 in dim2[i])
+                        {
+                            if (Convert.ToInt32(item2) >= 0)
+                            {
+                                var x1 = Convert.ToInt32(dim1[1].Replace(" ", ""));
+                                var x2 = Convert.ToInt32(dim1[2].Replace(" ", ""));
+                                var y1 = Convert.ToInt32(dim2[1].Replace(" ", ""));
+                                var y2 = int.Parse(dim2[2].Replace(" ", ""));
+
+                                Console.WriteLine(x1 + x2 + y1 + y2);
+                            }
+                        }
+                    }
+                }
+
+            }
+
+
+
+
+
+        }
+    }
+}
