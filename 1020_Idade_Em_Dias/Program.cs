7b1c369f4a17a338cf1cb5a96ca4adcb16c3fdbb
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2021-12-23 13:41:58 -0300

    1020

---
+++ 1020_Idade_Em_Dias/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1020_Idade_Em_Dias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var idade = int.Parse(Console.ReadLine());
+
+            // 1 = 400 / 365
+            var ano = idade / 365;
+            // 1 = (400 % 365) / 30
+            var mes = (idade % 365) / 30;
+            // 5 = (400 % 365) % 30
+            var dia = (idade % 365) % 30;
+
+            Console.WriteLine($"{ano} ano(s)");
+            Console.WriteLine($"{mes} mes(es)");
+            Console.WriteLine($"{dia} dia(s)");
+        }
+    }
+}
